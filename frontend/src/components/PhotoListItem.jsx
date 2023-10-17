f35dfcd6a5bb11c5311dbe45c72b3923ab1fd382
markmdoza <marklmdoza@gmail.com>
2023-10-17 18:52:25 -0400

    Building a component using props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,38 +2,24 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   return (
     <div className="imageSource">
-      <img src={sampleDataForPhotoListItem.imageSource}/>
+      <img src={props.data.imageSource}/>
       <div>
-        <img src={sampleDataForPhotoListItem.profile} alt="Profile Avatar" />
+        <img src={props.data.profile} alt="Profile Avatar" />
       </div>
       <div className="username">
         <span>
-        {sampleDataForPhotoListItem.username}
+          {props.data.username}
         </span>
         <div className="location">
-        {sampleDataForPhotoListItem.location.city}
-        {sampleDataForPhotoListItem.location.country}
+          {props.data.location.city}
+          {props.data.location.country}
         </div>
       </div>
     </div>
   )
 };
-
-console.log();
 
 export default PhotoListItem;
