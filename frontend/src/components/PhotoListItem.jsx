6b4c675c8a120fa2e2b54619af8f68f7a2adffff
markmdoza <marklmdoza@gmail.com>
2023-10-17 18:28:55 -0400

    Build a component using sample data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -15,7 +15,25 @@ const sampleDataForPhotoListItem = {
 };
 
 const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <div className="imageSource">
+      <img src={sampleDataForPhotoListItem.imageSource}/>
+      <div>
+        <img src={sampleDataForPhotoListItem.profile} alt="Profile Avatar" />
+      </div>
+      <div className="username">
+        <span>
+        {sampleDataForPhotoListItem.username}
+        </span>
+        <div className="location">
+        {sampleDataForPhotoListItem.location.city}
+        {sampleDataForPhotoListItem.location.country}
+        </div>
+      </div>
+    </div>
+  )
 };
+
+console.log();
 
 export default PhotoListItem;
