fbfb047e08128dbe9ea1ca92e93b2d86a0cff22f
markmdoza <marklmdoza@gmail.com>
2023-10-19 17:45:19 -0400

    Remove data from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
 
 const App = () => {
-  const photoList = [];
-
-  for (let i = 0; i < 3; i++) {
-    const sampleDataForPhotoListItem = {
-      id: `${i + 1}`,
-      location: {
-        city: "Montreal",
-        country: "Canada",
-      },
-      imageSource: `${process.env.PUBLIC_URL}/Image-${i + 1}-Regular.jpeg`,
-      username: 'Joe Example',
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    };
-
-    photoList.push(
-      <PhotoListItem key={sampleDataForPhotoListItem.id} data={sampleDataForPhotoListItem}/>
-    )
-  }
+  // const photoList = [];
+  // for (let i = 0; i < 3; i++) {
+  //   photoList.push(
+  //     <PhotoListItem key={sampleDataForPhotoListItem.id} data={sampleDataForPhotoListItem}/>
+  //   )
+  // }
   return (
     <div className="App">
-      {photoList}
+      <PhotoList />
     </div>
   );
 };
