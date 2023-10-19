2c6750f34705404606924bc9a509a4039925a5a0
markmdoza <marklmdoza@gmail.com>
2023-10-19 15:31:17 -0400

    Add the favorite icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,33 @@
 import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
+import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
-
-// Move the sampleDataForPhotoListItem object into your App.jsx component, and delete it from PhotoListItem.jsx.
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photoList = [];
+
+  for (let i = 0; i < 3; i++) {
+    const sampleDataForPhotoListItem = {
+      id: `${i + 1}`,
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      imageSource: `${process.env.PUBLIC_URL}/Image-${i + 1}-Regular.jpeg`,
+      username: 'Joe Example',
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    };
+
+    photoList.push(
+      <PhotoListItem key={sampleDataForPhotoListItem.id} data={sampleDataForPhotoListItem}/>
+    )
+  }
   return (
     <div className="App">
-      <PhotoListItem 
-        data={sampleDataForPhotoListItem}
-      />
+      {photoList}
+      {PhotoFavButton}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,23 +4,22 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div className="photo-list__item">
+    <section className="photo-list__item">
       <div>
         <img src={props.data.imageSource} className="photo-list__image"/>
       </div>
+      <section className="photo-list__user-info ">
       <div className="photo-list__user-details">
         <img src={props.data.profile} alt="Profile Avatar" className="photo-list__user-profile" />
-      </div>
-      <div className="photo-list__user-details">
-        <span className="photo-list__user-info">
-          {props.data.username}
-        </span>
-        <div className="photo-list__user-location">
-          {props.data.location.city}
-          {props.data.location.country}
+        <div className="photo-list__user-details">
+        {props.data.username}
+          <div className="photo-list__user-location">
+          {props.data.location.city}, {props.data.location.country}
+          </div>
         </div>
       </div>
-    </div>
+      </section>
+    </section>
   )
 };
 
