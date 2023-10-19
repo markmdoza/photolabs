65dff8e353ac262175bdd4be42860b5c099a4cc1
markmdoza <marklmdoza@gmail.com>
2023-10-18 23:13:22 -0400

    Apply styling to the photo component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,15 +5,17 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   return (
     <div className="imageSource">
-      <img src={props.data.imageSource}/>
+      <div className="photo-list__item">
+        <img src={props.data.imageSource} className="photo-list__image"/>
+      </div>
       <div>
-        <img src={props.data.profile} alt="Profile Avatar" />
+        <img src={props.data.profile} alt="Profile Avatar" className="photo-list__user-profile" />
       </div>
       <div className="username">
-        <span>
+        <span className="photo-list__user-info">
           {props.data.username}
         </span>
-        <div className="location">
+        <div className="photo-list__user-location">
           {props.data.location.city}
           {props.data.location.country}
         </div>
